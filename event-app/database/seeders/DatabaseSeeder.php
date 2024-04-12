0c9b32e8b19370115f0ed1ad34ff83efeba59503
kyaw Zaw Thwin <kyawzawthwin21@gmail.com>
2024-04-12 15:04:30 +0630

    laravel10

---
+++ event-app/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Database\Seeders;
+use App\Models\City;
+use App\Models\Country;
+use App\Models\Tag;
+use App\Models\User;
+
+
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        Country::create(['name' => 'United Kindom']);
+        Country::create(['name' => 'Myanmar']);
+        City::create(['country_id' => 1, 'name' => 'india']);
+        City::create(['country_id' => 1, 'name' => 'Liverpool']);
+        City::create(['country_id' => 1, 'name' => 'Leicester']);
+        City::create(['country_id' => 2, 'name' => 'Athens']);
+        City::create(['country_id' => 2, 'name' => 'Patra']);
+        City::create(['country_id' => 2, 'name' => 'Zakynthos']);
+
+        Tag::create(['name' => 'Laravel', 'slug' => 'laravel']);
+        Tag::create(['name' => 'Vue JS', 'slug' => 'react-js']);
+        Tag::create(['name' => 'Livewire', 'slug' => 'livewire']);
+    }
+}
+
+
+
---
+++ event-app/routes/web.php
@@ -0,0 +1,70 @@
+<?php
+
+use App\Http\Controllers\AttendingSystemController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\EventShowController;
+use App\Http\Controllers\LikeSystemController;
+use App\Http\Controllers\SaveSystemController;
+use App\Http\Controllers\StoreCommentController;
+use App\Http\Controllers\DeleteCommentController;
+use App\Http\Controllers\EventIndexController;
+use App\Http\Controllers\GalleryIndexController;
+use App\Http\Controllers\LikedEventController;
+use App\Http\Controllers\SavedEventController;
+use App\Http\Controllers\AttendingEventController;
+use App\Models\Country;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',WelcomeController::class )->name('welcome');
+Route::get('/e/{id}', EventShowController::class)->name('eventShow');
+Route::get('/e', EventIndexController::class)->name('eventIndex');
+Route::get('/gallery', GalleryIndexController::class)->name('galleryIndex');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/liked-events', LikedEventController::class)->name('likedEvents');
+    Route::get('/saved-events', SavedEventController::class)->name('savedEvents');
+    Route::get('/attendind-events', AttendingEventController::class)->name('attendingEvents');
+
+    Route::resource('/events', EventController::class);
+    Route::resource('/galleries', GalleryController::class);
+    Route::post('/events-like/{id}',LikeSystemController::class)->name('events.like');
+    Route::post('/events-save/{id}',SaveSystemController::class)->name('events.save');
+    Route::post('/events-attending/{id}',AttendingSystemController::class)->name('events.attending');
+
+    Route::post('/events/{id}/comments',StoreCommentController::class)->name('events.comments');
+    Route::delete('/events/{id}/comments/{comment}', DeleteCommentController::class)->name('events.comments.destroy');
+
+    Route::get('/countries/{country}', function (Country $country) {
+        return response()->json($country->cities);
+    });
+
+   
+
+
+});
+
+require __DIR__.'/auth.php';
+
